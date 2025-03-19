92dd2bade36feaf7d45691d73772f1f523224c2e
jchukwuezi <jchukwuezi@gmail.com>
2025-03-19 00:08:39 +0000

    configuring this for cloudflare

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@ const nextConfig = {
   // Enable Cloudflare compatibility
   experimental: {
     optimizeCss: true,
+    appDir: true,
   },
   // Use Cloudflare compatible runtime
   output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig 
